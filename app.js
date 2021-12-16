73040300fc14bac4ddcc260fd9c0287253b29de2
Jay Lee <jayleejay@seas.upenn.edu>
2021-12-16 11:44:06 -0800

    updated scheduler

--- app.js
+++ app.js
@@ -35,25 +35,6 @@ const io = require("./util/socket").init(server);
 
 app.use(express.json());
 
-const job = new CronJob("00 00 00 * * *", async () => {
-  // https://github.com/kelektiv/node-cron/blob/master/examples/at_midnight.js
-  // delete any data older than 24 hours at midnight
-  const d = new Date();
-  console.log("Midnight:", d);
-
-  try {
-    let yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
-    let deleteCount = await User.deleteMany({ time: { $lte: yesterday } });
-    console.log("Old user data deleted: " + deleteCount);
-    deleteCount = await Game.deleteMany({ time: { $lte: yesterday } });
-    console.log("Old game data deleted: " + deleteCount);
-  } catch (err) {
-    return next(new HttpError("Old user/game data deletion fail " + err), 500);
-  }
-});
-job.start();
-
 // CORS allow
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
---
+++ util/scheduled-jobs.js
@@ -0,0 +1,39 @@
+const User = require("./models/user");
+const Game = require("../models/game");
+
+// const job = new CronJob("00 00 00 * * *", async () => {
+//     // https://github.com/kelektiv/node-cron/blob/master/examples/at_midnight.js
+//     // delete any data older than 24 hours at midnight
+//     const d = new Date();
+//     console.log("Midnight:", d);
+
+//     try {
+//       let yesterday = new Date();
+//       yesterday.setDate(yesterday.getDate() - 1);
+//       let deleteCount = await User.deleteMany({ time: { $lte: yesterday } });
+//       console.log("Old user data deleted: " + deleteCount);
+//       deleteCount = await Game.deleteMany({ time: { $lte: yesterday } });
+//       console.log("Old game data deleted: " + deleteCount);
+//     } catch (err) {
+//       return next(new HttpError("Old user/game data deletion fail " + err), 500);
+//     }
+//   });
+//   job.start();
+
+const cleanUpDatabase = async () => {
+  // https://github.com/kelektiv/node-cron/blob/master/examples/at_midnight.js
+  // delete any data older than 24 hours at midnight
+  const d = new Date();
+  try {
+    let yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    let deleteCount = await User.deleteMany({ time: { $lte: yesterday } });
+    console.log("Old user data deleted: " + deleteCount);
+    deleteCount = await Game.deleteMany({ time: { $lte: yesterday } });
+    console.log("Old game data deleted: " + deleteCount);
+  } catch (err) {
+    return next(new HttpError("Old user/game data deletion fail " + err), 500);
+  }
+};
+
+cleanUpDatabase();
