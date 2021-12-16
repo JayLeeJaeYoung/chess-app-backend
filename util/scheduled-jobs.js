fb047389337aecd82f18f2757d07ffab79b632ab
Jay Lee <jayleejay@seas.upenn.edu>
2021-12-16 12:06:14 -0800

    updated

--- util/scheduled-jobs.js
+++ util/scheduled-jobs.js
@@ -1,4 +1,4 @@
-const User = require("./models/user");
+const User = require("../models/user");
 const Game = require("../models/game");
 
 // const job = new CronJob("00 00 00 * * *", async () => {
